11ba84f57f398476ab09369f8a3e541f3bbb8680
Arthur-Neto <arthursan.neto@hotmail.com>
2021-07-01 13:44:58 -0300

    Configurado testes unitarios para build do frontend

--- src/Website/Template.Client/karma-ci.conf.js
+++ src/Website/Template.Client/karma-ci.conf.js
@@ -8,8 +8,8 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
             require('karma-coverage'),
+            require('karma-junit-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
@@ -24,18 +24,32 @@ module.exports = function (config) {
         jasmineHtmlReporter: {
             suppressAll: true, // removes the duplicated traces
         },
+        junitReporter: {
+            outputDir: './coverage/',
+        },
         coverageReporter: {
             dir: require('path').join(__dirname, './coverage/'),
             subdir: '.',
-            reporters: [{ type: 'html' }, { type: 'text-summary' }],
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' },
+                { type: 'cobertura' },
+            ],
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'junit'],
         port: 9876,
         colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['Chrome'],
-        singleRun: false,
+        logLevel: config.LOG_ERROR,
+        autoWatch: false,
+        browsers: ['ChromeHeadlessCI'],
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox'],
+            },
+        },
+        singleRun: true,
+        browserNoActivityTimeout: 40000,
         restartOnFileChange: true,
     });
 };
